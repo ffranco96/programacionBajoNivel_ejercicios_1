93d3714d94d14ae185f00bdd1999d942b66965a4
Franco <macen38787@estudiantes.untref.edu.ar>
2023-04-22 16:20:39 -0300

    Adding configuration files and main to my new project part 2.

---
+++ cmake-build-debug/CMakeFiles/programacionBajoNivel_ejercicios_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programacionBajoNivel_ejercicios_1.dir/main.c.obj"
+  "CMakeFiles/programacionBajoNivel_ejercicios_1.dir/main.c.obj.d"
+  "libprogramacionBajoNivel_ejercicios_1.dll.a"
+  "programacionBajoNivel_ejercicios_1.exe"
+  "programacionBajoNivel_ejercicios_1.exe.manifest"
+  "programacionBajoNivel_ejercicios_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/programacionBajoNivel_ejercicios_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
